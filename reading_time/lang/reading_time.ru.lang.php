4a9fa7c80ad9bbd78667de5ce1eeb4ea999d257c
seditio <support@seditio.by>
2025-12-29 22:47:20 +0300

    Comment fix

--- reading_time/lang/reading_time.ru.lang.php
+++ reading_time/lang/reading_time.ru.lang.php
@@ -16,6 +16,10 @@ defined('COT_CODE') or die('Wrong URL');
 $L['info_name'] = '[SEDBY] Reading Time';
 $L['info_desc'] = 'Отображение примерного времени на прочтение страницы';
 
+/**
+ * Plugin Config
+ */
+
 $L['cfg_fields_plain'] = 'Список полей и экстраполей с текстом без разметки';
 $L['cfg_fields_plain_hint'] = 'Очистить для пропуска';
 $L['cfg_fields_html'] = 'Список полей и экстраполей с HTML-текстом';
